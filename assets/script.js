ca958f6c6c0b9e2e1b38ab99537cfae96c8e0411
ebartlik <ebartlik@gmail.com>
2018-07-07 12:18:38 -0400

    js 12:18

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,3 @@
-// src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"
-// src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-
-
-
 var config = {
     apiKey: "AIzaSyA34rCupaiOnfXRKBhsmgYsTc6Brp9ysew",
     authDomain: "beers-and-bands-rev-1.firebaseapp.com",
@@ -16,25 +11,43 @@ var config = {
 
 
 
-
+var artist = $('locationInput').val();
   
   var dataRef = firebase.database();
+// var artist = "Drake";
+// var location = "New+York"
 
 
+  var proxyURL = 'https://shielded-hamlet-43668.herokuapp.com/';
   var queryURL1 = 'http://beermapping.com/webservice/loccity/1cd5db8a402574bb7ecab4285b88793f/jersey+city&s=json'
+  // var queryURL1 = 'http://beermapping.com/webservice/loccity/1cd5db8a402574bb7ecab4285b88793f/jersey+city&s=json'
   var queryURL2 = 'http://rest.bandsintown.com/artists/maroon+5/events?app_id=1f631e0138387b85bc49eb32aa23ea48&artistname=maroon+5'
+  //var queryURL2 = 'http://rest.bandsintown.com/artists/' + artist + '/events?app_id=1f631e0138387b85bc49eb32aa23ea48&artistname=' + artist
 
   $.ajax({
-    url: queryURL1,
+    url: proxyURL + queryURL1,
     method: "GET",
     dataType: "xml",
   }).then(function(response) {});
 
   $.ajax({
-    url: queryURL2,
+    url: proxyURL + queryURL2,
     method: "GET",
     dataType: "xml",
   }).then(function(response) {});
+
+
+  // $("#letsRock").on("click", function(event) {
+  //   // prevent form from submitting
+  //   event.preventDefault();
+    
+  //   console.log(artist);
+
+  //   $('beer-holder').html(artist);
+    
+  // });
+
+    
 
 
   // }, function(errorObject) {
